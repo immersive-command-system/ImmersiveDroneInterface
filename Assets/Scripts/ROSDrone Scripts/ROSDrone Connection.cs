ba67bd727d2c118d925c3eb3877726c92809070d
Jesse Paterson <jesse.r.pat@gmail.com>
2017-11-06 10:39:55 -0800

    Getting ROS set up

---
+++ Assets/Scripts/ROSDrone Scripts/ROSDrone Connection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ROSBridgeLib;
+
+public class ROSDroneConnection : MonoBehaviour {
+    private ROSBridgeWebSocketConnection ros = null;
+
+    void Start()
+    {
+        ros = new ROSBridgeWebSocketConnection("192.168.10.4", 9090);
+        ros.AddSubscriber(typeof(ROSBridgeSubscriber));
+        ros.AddServiceResponse(typeof(RosDroneServiceResponse));
+        ros.Connect();
+    }
+
+    // Extremely important to disconnect from ROS. OTherwise packets continue to flow
+    void OnApplicationQuit()
+    {
+        if (ros != null)
+        {
+            ros.Disconnect();
+        }
+    }
+
+    // Update is called once per frame in Unity
+    void Update()
+    {
+        ros.Render();
+    }
+}
---
+++ Assets/Scripts/ROSDrone Scripts/RosDroneServiceResponse.cs
@@ -0,0 +1,28 @@
+﻿using ROSBridgeLib;
+using System.Collections;
+using SimpleJSON;
+using UnityEngine;
+
+/**
+ * This is a toy example of the Unity-ROS interface talking to the TurtleSim 
+ * tutorial (circa Groovy). Note that due to some changes since then this will have
+ * to be slightly re-written, but as its a test ....
+ * 
+ * This defines the callback that responds to service requests. Try the 'T' character
+ * to toggle the line left by the turtle.
+ * 
+ * @author Michael Jenkin, Robert Codd-Downey and Andrew Speers
+ * @version 3.0
+ **/
+
+public class RosDroneServiceResponse
+{
+
+    public static void ServiceCallBack(string service, string response)
+    {
+        if (response == null)
+            Debug.Log("ServiceCallback for service " + service);
+        else
+            Debug.Log("ServiceCallback for service " + service + " response " + response);
+    }
+}
