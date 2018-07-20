43cfef64283eb7575f8fc840a59455f53726d7b7
Jessica Lee <jessica.xie.lee@gmail.com>
2018-07-20 14:14:31 -0700

    added torus game object

--- Assets/Scripts/ROS Scripts/ObstacleSubscriber.cs
+++ Assets/Scripts/ROS Scripts/ObstacleSubscriber.cs
@@ -1,45 +1,48 @@
-﻿using ROSBridgeLib;
-using ROSBridgeLib.std_msgs;
-using ROSBridgeLib.interface_msgs;
-using System.Collections;
-using System.Collections.Generic;
-using SimpleJSON;
-using UnityEngine;
-using System.IO;
-
-public class ObstacleSubscriber : ROSBridgeSubscriber
+﻿namespace ISAACS
 {
-    public static HashSet<int> ids = new HashSet<int>();
+    using ROSBridgeLib;
+    using ROSBridgeLib.std_msgs;
+    using ROSBridgeLib.interface_msgs;
+    using System.Collections;
+    using System.Collections.Generic;
+    using SimpleJSON;
+    using UnityEngine;
+    using System.IO;
 
-    public static string GetMessageTopic()
+    public class ObstacleSubscriber : ROSBridgeSubscriber
     {
-        return "/vis/true_env";
-    }
-
-    public static string GetMessageType()
-    {
-        return "visualization_msgs/Marker";
-    }
-
-    public static ROSBridgeMsg ParseMessage(JSONNode msg)
-    {
-        return new ObstacleMsg(msg);
-    }
-
-    public static void CallBack(ROSBridgeMsg msg)
-    {
-        //Debug.Log("callback");
-        Vector3 tablePos = GameObject.FindWithTag("Table").transform.position;
-        ObstacleMsg pose = (ObstacleMsg)msg;
-        if (!ids.Contains(pose.id) && pose.id!=0)
+        public static HashSet<int> ids = new HashSet<int>();
+        public static GameObject torus = (GameObject)WorldProperties.worldObject.GetComponent<WorldProperties>().torus;
+        public static string GetMessageTopic()
         {
-            Debug.Log("making sphere id:" + pose.id);
-            ids.Add(pose.id);
-            GameObject world = GameObject.FindWithTag("World");
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            sphere.transform.parent = world.transform;
-            sphere.transform.localPosition = new Vector3(-pose._x, pose._z + tablePos.z + 0.148f, -pose._y);
-            sphere.transform.localScale = new Vector3(pose.scale_x, pose.scale_x, pose.scale_x) / 5;
+            return "/vis/true_env";
+        }
+
+        public static string GetMessageType()
+        {
+            return "visualization_msgs/Marker";
+        }
+
+        public static ROSBridgeMsg ParseMessage(JSONNode msg)
+        {
+            return new ObstacleMsg(msg);
+        }
+
+        public static void CallBack(ROSBridgeMsg msg)
+        {
+            //Debug.Log("callback");
+            Vector3 tablePos = GameObject.FindWithTag("Table").transform.position;
+            ObstacleMsg pose = (ObstacleMsg)msg;
+            if (!ids.Contains(pose.id) && pose.id != 0)
+            {
+                Debug.Log("making sphere id:" + pose.id);
+                ids.Add(pose.id);
+                GameObject world = GameObject.FindWithTag("World");
+                Object.Instantiate(torus);
+                torus.transform.parent = world.transform;
+                torus.transform.localPosition = new Vector3(-pose._x, pose._z + tablePos.z + 0.148f, -pose._y);
+                torus.transform.localScale = new Vector3(pose.scale_x, pose.scale_x, pose.scale_x) / 5;
+            }
         }
     }
 }
