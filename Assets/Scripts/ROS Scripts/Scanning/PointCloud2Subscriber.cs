03cd251b3d78f28cb0b6b2af7822df9ba78ab512
Eric <ejyzhang@berkeley.edu>
2018-11-15 17:04:57 -0800

    Added PointCloud2 subscriber and new connection script

---
+++ Assets/Scripts/ROS Scripts/Scanning/PointCloud2Subscriber.cs
@@ -0,0 +1,29 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.sensor_msgs;
+using SimpleJSON;
+using UnityEngine;
+
+public class PointCloud2Subscriber : ROSBridgeSubscriber
+{
+
+    public new static string GetMessageTopic()
+    {
+        return "velodyne/velodyne_points";
+    }
+
+    public new static string GetMessageType()
+    {
+        return "sensor_msgs/PointCloud2";
+    }
+
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        return new PointCloud2Msg(msg);
+    }
+
+    public new static void CallBack(ROSBridgeMsg msg)
+    {
+        PointCloud2Msg pointCloudMsg = (PointCloud2Msg)msg;
+        Debug.Log(pointCloudMsg.GetHeader().GetSeq());
+    }
+}
---
+++ Assets/Scripts/ROS Scripts/Scanning/ScanningConnection.cs
@@ -0,0 +1,33 @@
+﻿using ROSBridgeLib;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanningConnection : MonoBehaviour {
+
+    private ROSBridgeWebSocketConnection ros = null;
+
+    // Use this for initialization
+    void Start () {
+        // TODO: Understand this line
+        Debug.Log("Starting Scanning Connection...");
+        ros = new ROSBridgeWebSocketConnection("ws://128.32.43.94", 9090);
+        ros.AddSubscriber(typeof(PointCloud2Subscriber));
+        ros.Connect();
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Disconnecting from ROS");
+        if (ros != null)
+        {
+            ros.Disconnect();
+        }
+    }
+
+    // Update is called once per frame in Unity
+    void Update()
+    {
+        ros.Render();
+    }
+}
