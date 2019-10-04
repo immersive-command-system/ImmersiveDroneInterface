1f14723214de980532538b403b20e35bb2db5b40
Ji Han <jihan2311@gmail.com>
2019-10-04 15:00:11 -0700

    add subscriber for error bound

---
+++ Assets/Scripts/ROS Scripts/TrajectoryBoundMsg.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Text;
+using SimpleJSON;
+using ROSBridgeLib.std_msgs;
+
+namespace ROSBridgeLib
+{
+    namespace interface_msgs
+    {
+        public class TrajectoryBoundMsg : ROSBridgeMsg
+        {
+            public float _x, _y, _z;
+            public int id;
+            public string ns;
+            public float _a, _r, _g, _b;
+
+            public TrajectoryBoundMsg(JSONNode msg)
+            {
+
+                ns = msg["ns"];
+                //x,y,z are scaled
+                _x = float.Parse(msg["scale"]["x"]);
+                _y = float.Parse(msg["scale"]["y"]);
+                _z = float.Parse(msg["scale"]["z"]);
+                //rgb color for visualization
+                _a = float.Parse(msg["color"]["a"]);
+                _r = float.Parse(msg["color"]["r"]);
+                _g = float.Parse(msg["color"]["g"]);
+                _b = float.Parse(msg["color"]["b"]);
+
+                id = int.Parse(msg["id"]); ;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ROS Scripts/TrajectoryBoundSubscriber.cs
@@ -0,0 +1,45 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.std_msgs;
+using ROSBridgeLib.interface_msgs;
+using System.Collections;
+using SimpleJSON;
+using UnityEngine;
+using ISAACS;
+using System.IO;
+using UnityEditor;
+
+public class TrajectoryBoundSubscriber : ROSBridgeSubscriber
+{
+
+    public new static string GetMessageTopic()
+    {
+        return "/vis/bound";
+    }
+
+    public new static string GetMessageType()
+    {
+        //New message type needed
+        return "crazyflie_msgs/PositionVelocityStateStamped";
+    }
+
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        return new TrajectoryBoundMsg(msg);
+    }
+
+    public new static void CallBack(ROSBridgeMsg msg)
+    {
+        Debug.Log("Bounding box callback");
+        GameObject drone = GameObject.FindWithTag("Drone");
+        if (drone != null)
+        {
+            //Get waypoints, get lines from waypoints, apply box from msg radius
+        }
+        else
+        {
+            Debug.Log("Can't find drone");
+        }
+    }
+  
+
+}
