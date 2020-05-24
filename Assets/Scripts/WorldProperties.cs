02fe1de69ad0a3cf9b5e2a987a19cab23e979ae1
PeruDayani <perudayani@berkeley.edu>
2020-05-24 14:27:35 -0700

    dummy waypoint generation

--- Assets/Scripts/WorldProperties.cs
+++ Assets/Scripts/WorldProperties.cs
@@ -84,8 +84,8 @@
         private static bool missionActive = false;
         private static bool waypointMissionUploading = false;
         private static bool inFlight = false;
+        private static int nextWaypointID = 0;
         private static int currentWaypointID = 0;
-        private static int prevWaypointID = 0;
 
         // Use this for initialization
         void Start()
@@ -180,6 +180,16 @@
         /// </summary>
         public static void StartDroneMission()
         {
+
+            ArrayList waypoints = WorldProperties.selectedDrone.waypoints;
+
+            /// Check if atleast one waypoint has been generated
+            if (waypoints.Count < 2)
+            {
+                Debug.Log("ALERT: Not enough waypoints set to start mission");
+                return;
+            }    
+
             // Create and execute waypoint mission of first two waypoints
             List<MissionWaypointMsg> missionMissionMsgList = new List<MissionWaypointMsg>();
 
@@ -192,34 +202,63 @@
                 command_params[i] = 0;
             }
 
-            ArrayList waypoints = WorldProperties.selectedDrone.waypoints;
-
             // Waypoint 0 is disregared as it is the act of taking off.
             Waypoint waypoint_0 = (Waypoint)waypoints[0];
+            float prev_waypoint_x = waypoint_0.gameObjectPointer.transform.localPosition.x;
+            float prev_waypoint_y = waypoint_0.gameObjectPointer.transform.localPosition.y;
+            float prev_waypoint_z = waypoint_0.gameObjectPointer.transform.localPosition.z;
+
 
             // Waypoint 1 is the first waypoint mission.
             Waypoint waypoint_1 = (Waypoint)waypoints[1];
+            float waypoint_x = waypoint_1.gameObjectPointer.transform.localPosition.x;
+            float waypoint_y = waypoint_1.gameObjectPointer.transform.localPosition.y;
+            float waypoint_z = waypoint_1.gameObjectPointer.transform.localPosition.z;
 
-            // TODO: Generate dummy waypoints for the mission
+            double waypoint_ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, waypoint_x);
+            float waypoint_ROS_y = (waypoint_y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
+            double waypoint_ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, waypoint_z);
 
-            float x = waypoint_1.gameObjectPointer.transform.localPosition.x;
-            float y = waypoint_1.gameObjectPointer.transform.localPosition.y;
-            float z = waypoint_1.gameObjectPointer.transform.localPosition.z;
+            MissionWaypointMsg new_waypoint = new MissionWaypointMsg(waypoint_ROS_x, waypoint_ROS_z, waypoint_ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
 
-            double ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, x);
-            float ROS_y = (y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
-            double ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, z);
+            // Generate dummy waypoint 1
 
-            MissionWaypointMsg new_waypoint = new MissionWaypointMsg(ROS_x, ROS_z, ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
-            Debug.Log("single waypoint info: " + new_waypoint);
+            float dummy_1_waypoint_x = (waypoint_x + prev_waypoint_x) * (1 / 3);
+            float dummy_1_waypoint_y = (waypoint_y + prev_waypoint_y) * (1 / 3);
+            float dummy_1_waypoint_z = (waypoint_z + prev_waypoint_z) * (1 / 3);
+
+            double dummy_1_waypoint_ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, dummy_1_waypoint_x);
+            float dummy_1_waypoint_ROS_y = (dummy_1_waypoint_y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
+            double dummy_1_waypoint_ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, dummy_1_waypoint_z);
+
+            MissionWaypointMsg dummy_1_waypoint = new MissionWaypointMsg(dummy_1_waypoint_ROS_x, dummy_1_waypoint_ROS_z, dummy_1_waypoint_ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
+
+            // Generate dummy waypoint 2
+
+            float dummy_2_waypoint_x = (waypoint_x + prev_waypoint_x) * (2 / 3); ;
+            float dummy_2_waypoint_y = (waypoint_y + prev_waypoint_y) * (2 / 3); ;
+            float dummy_2_waypoint_z = (waypoint_z + prev_waypoint_z) * (2 / 3); ;
+
+            double dummy_2_waypoint_ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, dummy_2_waypoint_x);
+            float dummy_2_waypoint_ROS_y = (dummy_2_waypoint_y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
+            double dummy_2_waypoint_ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, dummy_2_waypoint_z);
+
+            MissionWaypointMsg dummy_2_waypoint = new MissionWaypointMsg(dummy_2_waypoint_ROS_x, dummy_2_waypoint_ROS_z, dummy_2_waypoint_ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
+
+            Debug.Log("Adding waypoint mission: " + new_waypoint);
+
+            // Create and upload waypoint mission
+
+            missionMissionMsgList.Add(dummy_1_waypoint);
+            missionMissionMsgList.Add(dummy_2_waypoint);
             missionMissionMsgList.Add(new_waypoint);
 
             MissionWaypointTaskMsg Task = new MissionWaypointTaskMsg(15.0f, 15.0f, MissionWaypointTaskMsg.ActionOnFinish.NO_ACTION, 1, MissionWaypointTaskMsg.YawMode.AUTO, MissionWaypointTaskMsg.TraceMode.POINT, MissionWaypointTaskMsg.ActionOnRCLost.FREE, MissionWaypointTaskMsg.GimbalPitchMode.FREE, missionMissionMsgList.ToArray());
             worldObject.GetComponent<ROSDroneConnection>().UploadMission(Task);
 
             // Start loop to check and publish waypoints
+            nextWaypointID = 2;
             currentWaypointID = 1;
-            prevWaypointID = 0;
             missionActive = true;
             waypointMissionUploading = true;
         }
@@ -230,6 +269,21 @@
         /// </summary>
         public static void UploadNextWaypointMission()
         {
+            ArrayList waypoints = WorldProperties.selectedDrone.waypoints;
+
+            /// Check if there is another waypoint
+            if (waypoints.Count == nextWaypointID)
+            {
+                Debug.Log("ALERT: All waypoints successfully send");
+                Debug.Log("ALERT: Drone is send home by default");
+
+                missionActive = false;
+                inFlight = false;
+                worldObject.GetComponent<ROSDroneConnection>().GoHome();
+
+                return;
+            }
+
             // Create and execute waypoint mission of first two waypoints
             List<MissionWaypointMsg> missionMissionMsgList = new List<MissionWaypointMsg>();
 
@@ -242,31 +296,63 @@
                 command_params[i] = 0;
             }
 
-            ArrayList waypoints = WorldProperties.selectedDrone.waypoints;
+            // Get the position of the last completed waypoint.
+            Waypoint prev_waypoint = (Waypoint)waypoints[currentWaypointID];
+            float prev_waypoint_x = prev_waypoint.gameObjectPointer.transform.localPosition.x;
+            float prev_waypoint_y = prev_waypoint.gameObjectPointer.transform.localPosition.y;
+            float prev_waypoint_z = prev_waypoint.gameObjectPointer.transform.localPosition.z;
 
-            // The next id to be send.
-            Waypoint next_waypoint = (Waypoint)waypoints[currentWaypointID + 1];
 
-            // TODO: Generate dummy waypoints for the mission
+            // Create a mission for the next user generated waypoint.
+            Waypoint next_waypoint = (Waypoint)waypoints[nextWaypointID];
+            float waypoint_x = next_waypoint.gameObjectPointer.transform.localPosition.x;
+            float waypoint_y = next_waypoint.gameObjectPointer.transform.localPosition.y;
+            float waypoint_z = next_waypoint.gameObjectPointer.transform.localPosition.z;
 
-            float x = next_waypoint.gameObjectPointer.transform.localPosition.x;
-            float y = next_waypoint.gameObjectPointer.transform.localPosition.y;
-            float z = next_waypoint.gameObjectPointer.transform.localPosition.z;
+            double waypoint_ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, waypoint_x);
+            float waypoint_ROS_y = (waypoint_y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
+            double waypoint_ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, waypoint_z);
 
-            double ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, x);
-            float ROS_y = (y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
-            double ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, z);
+            MissionWaypointMsg new_waypoint = new MissionWaypointMsg(waypoint_ROS_x, waypoint_ROS_z, waypoint_ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
 
-            MissionWaypointMsg new_waypoint = new MissionWaypointMsg(ROS_x, ROS_z, ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
-            Debug.Log("single waypoint info: " + new_waypoint);
+            // Generate dummy waypoint 1
+
+            float dummy_1_waypoint_x = (waypoint_x + prev_waypoint_x) * (1 / 3);
+            float dummy_1_waypoint_y = (waypoint_y + prev_waypoint_y) * (1 / 3);
+            float dummy_1_waypoint_z = (waypoint_z + prev_waypoint_z) * (1 / 3);
+
+            double dummy_1_waypoint_ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, dummy_1_waypoint_x);
+            float dummy_1_waypoint_ROS_y = (dummy_1_waypoint_y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
+            double dummy_1_waypoint_ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, dummy_1_waypoint_z);
+
+            MissionWaypointMsg dummy_1_waypoint = new MissionWaypointMsg(dummy_1_waypoint_ROS_x, dummy_1_waypoint_ROS_z, dummy_1_waypoint_ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
+
+            // Generate dummy waypoint 2
+
+            float dummy_2_waypoint_x = (waypoint_x + prev_waypoint_x) * (2 / 3); ;
+            float dummy_2_waypoint_y = (waypoint_y + prev_waypoint_y) * (2 / 3); ;
+            float dummy_2_waypoint_z = (waypoint_z + prev_waypoint_z) * (2 / 3); ;
+
+            double dummy_2_waypoint_ROS_x = WorldProperties.UnityXToLat(WorldProperties.droneHomeLat, dummy_2_waypoint_x);
+            float dummy_2_waypoint_ROS_y = (dummy_2_waypoint_y * WorldProperties.Unity_Y_To_Alt_Scale) - 1f;
+            double dummy_2_waypoint_ROS_z = WorldProperties.UnityZToLong(WorldProperties.droneHomeLong, WorldProperties.droneHomeLat, dummy_2_waypoint_z);
+
+            MissionWaypointMsg dummy_2_waypoint = new MissionWaypointMsg(dummy_2_waypoint_ROS_x, dummy_2_waypoint_ROS_z, dummy_2_waypoint_ROS_y, 3.0f, 0, 0, MissionWaypointMsg.TurnMode.CLOCKWISE, 0, 30, new MissionWaypointActionMsg(0, command_list, command_params));
+
+            Debug.Log("Adding waypoint mission: " + new_waypoint);
+
+            // Create and upload waypoint mission
+
+            missionMissionMsgList.Add(dummy_1_waypoint);
+            missionMissionMsgList.Add(dummy_2_waypoint);
             missionMissionMsgList.Add(new_waypoint);
 
             MissionWaypointTaskMsg Task = new MissionWaypointTaskMsg(15.0f, 15.0f, MissionWaypointTaskMsg.ActionOnFinish.NO_ACTION, 1, MissionWaypointTaskMsg.YawMode.AUTO, MissionWaypointTaskMsg.TraceMode.POINT, MissionWaypointTaskMsg.ActionOnRCLost.FREE, MissionWaypointTaskMsg.GimbalPitchMode.FREE, missionMissionMsgList.ToArray());
             worldObject.GetComponent<ROSDroneConnection>().UploadMission(Task);
 
             // Start loop to check and publish waypoints
-            prevWaypointID = currentWaypointID;
-            currentWaypointID = currentWaypointID + 1;
+            currentWaypointID = nextWaypointID;
+            nextWaypointID = nextWaypointID +1;
             waypointMissionUploading = true;
         }
 
