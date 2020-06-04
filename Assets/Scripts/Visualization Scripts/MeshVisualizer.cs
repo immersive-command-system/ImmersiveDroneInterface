96b222a424a85d52d749eab5be03011f314fc511
Eric Wang <erwang@berkeley.edu>
2020-06-03 18:47:25 -0700

    made mesh visualization more efficient

--- Assets/Scripts/Visualization Scripts/MeshVisualizer.cs
+++ Assets/Scripts/Visualization Scripts/MeshVisualizer.cs
@@ -12,11 +12,14 @@ public class MeshVisualizer : MonoBehaviour
 
     private bool hasChanged = false;
 
-    private Dictionary<Int64[], List<float[,]>> mesh_dict;    // Use this for initialization
+    private Dictionary<Int64[], MeshFilter> mesh_dict;    // Use this for initialization
     void Start()
     {
-        mesh_dict = new Dictionary<long[], List<float[,]>>();
-        meshParent = new GameObject("Initial Mesh");
+        mesh_dict = new Dictionary<long[], MeshFilter>();
+        meshParent = new GameObject("Mesh");
+        if (flipYZ) {
+            meshParent.transform.rotation = Quaternion.Euler(-90.0f, 0, 0);
+        }
     }
 
     // Update is called once per frame
@@ -43,104 +46,58 @@ public class MeshVisualizer : MonoBehaviour
         {
             Int64[] index = mesh_blocks[i].GetIndex();
 
+            List<Vector3> newVertices = new List<Vector3>();
+            // Also not sure what to do with all the newColors...
+            List<Color> newColors = new List<Color>();
+        
             UInt16[] x = mesh_blocks[i].GetX();
-            float[] pos_x = new float[x.Length];
-            for (int j = 0; j < pos_x.Length; j++)
-            {
-                pos_x[j] = (float)x[j] / 32768.0f + index[0] * scale_factor;
-            }
-
             UInt16[] y = mesh_blocks[i].GetY();
-            float[] pos_y = new float[y.Length];
-            for (int j = 0; j < pos_x.Length; j++)
-            {
-                pos_y[j] = (float)y[j] / 32768.0f + index[1] * scale_factor;
-            }
-
             UInt16[] z = mesh_blocks[i].GetZ();
-            float[] pos_z = new float[z.Length];
-            for (int j = 0; j < pos_z.Length; j++)
+            for (int j = 0; j < x.Length; j++)
             {
-                pos_z[j] = (float)z[j] / 32768.0f + index[2] * scale_factor;
+                float zv = (float)z[j] / 32768.0f + index[2] * scale_factor;
+                float xv = (float)x[j] / 32768.0f + index[0] * scale_factor;
+                float yv = (float)y[j] / 32768.0f + index[1] * scale_factor;
+                newVertices.Add(new Vector3(xv, yv, zv));
             }
-
             byte[] r = mesh_blocks[i].GetR();
             byte[] g = mesh_blocks[i].GetG();
             byte[] b = mesh_blocks[i].GetB();
 
-            float[,] pos_list = new float[3, pos_x.Length];
-            float[,] color_list = new float[3, r.Length];
-            for (int j = 0; j < pos_x.Length; j++)
-            {
-                pos_list[0, j] = pos_x[j];
-                pos_list[1, j] = pos_y[j];
-                pos_list[2, j] = pos_z[j];
-            }
             for (int j = 0; j < r.Length; j++)
             {
-                color_list[0, j] = (float)(r[j]);
-                color_list[1, j] = (float)(g[j]);
-                color_list[2, j] = (float)(b[j]);
+                newColors.Add(new Color32(r[j], g[j], b[j], 51));
             }
 
-            List<float[,]> data_list = new List<float[,]>();
-            data_list.Add(pos_list);
-            data_list.Add(color_list);
-            mesh_dict.Add(index, data_list);
-        }
+            //    mesh_dict.Add(index, data_list);
 
-        List<Vector3> newVertices = new List<Vector3>();
-        // Also not sure what to do with all the newColors...
-        List<Color> newColors = new List<Color>();
-        foreach (KeyValuePair<Int64[], List<float[,]>> entry in mesh_dict)
-        {
-            float[,] pos_list = entry.Value[0];
-            Vector3[] verts = new Vector3[pos_list.GetLength(1)];
-
-            for (int i = 0; i < verts.Length; i++)
+            int[] newTriangles = new int[newVertices.Count / 3 * 3];
+            for (int j = 0; j < newTriangles.Length; j++)
             {
-                verts[i] = new Vector3(pos_list[0, i], pos_list[1, i], pos_list[2, i]);
+                newTriangles[j] = j;
             }
 
-            newVertices.AddRange(verts);
-
-            float[,] color_list = entry.Value[1];
-            Color[] colors = new Color[color_list.GetLength(1)];
-
-            for (int i = 0; i < colors.Length; i++)
+            // TODO
+            Mesh mesh = new Mesh();
+            if (!mesh_dict.ContainsKey(index))
             {
-                colors[i] = new Color(color_list[0, i], color_list[1, i], color_list[2, i], 0.2f);
+                GameObject meshObject = new GameObject(index.ToString());
+                meshObject.transform.parent = meshParent.transform;
+                MeshFilter meshFilter = meshObject.AddComponent<MeshFilter>();
+                MeshRenderer meshRenderer = meshObject.AddComponent<MeshRenderer>();
+                meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+                mesh_dict.Add(index, meshFilter);
             }
+            mesh.vertices = newVertices.ToArray();
+            // ?
+            //mesh.uv = newUV;
+            // Also not sure if this is correct either... Python and Unity seem to disagree on this point.
+            mesh.triangles = newTriangles;
 
-            newColors.AddRange(colors);
+            // colors may not be the same lengths as vertices. Unity demands that it be the same as the vertices.
+            mesh.colors = newColors.ToArray();
+            mesh_dict[index].mesh = mesh;
         }
-
-        int[] newTriangles = new int[newVertices.Count / 3 * 3];
-        for (int i = 0; i < newTriangles.Length; i++)
-        {
-            newTriangles[i] = i;
-        }
-
-        Destroy(meshParent);
-        // Insert timestamp here maybe?
-        meshParent = new GameObject("Mesh");
-
-        MeshRenderer meshRenderer = meshParent.AddComponent<MeshRenderer>();
-        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
-
-        // TODO
-        Mesh mesh = new Mesh();
-        MeshFilter meshFilter = meshParent.AddComponent<MeshFilter>();
-        mesh.vertices = newVertices.ToArray();
-        // ?
-        //mesh.uv = newUV;
-        // Also not sure if this is correct either... Python and Unity seem to disagree on this point.
-        mesh.triangles = newTriangles;
-
-        // colors may not be the same lengths as vertices. Unity demands that it be the same as the vertices.
-        mesh.colors = newColors.ToArray();
-        meshFilter.mesh = mesh;
-
         hasChanged = true;
     }
 }
